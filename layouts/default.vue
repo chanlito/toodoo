27545b617f9e219d62509bec7c1d5633b6713c55
Chanlito Hem <chanlitohem@gmail.com>
2020-08-23 00:38:03 +0700

    refactor: update homepage & colors

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,12 @@
 <template>
   <v-app>
-    <v-app-bar app color="white" elevate-on-scroll>
+    <v-app-bar app elevate-on-scroll>
       <v-container class="d-flex align-center py-0 px-0 px-sm-0 px-md-4" style="max-width: 1080px;">
         <v-app-bar-nav-icon class="ml-n3 mr-1 hidden-sm-and-up" />
         <app-logo />
+        <v-spacer />
+        <v-btn class="mr-2" text><span class="text--primary">Log In</span></v-btn>
+        <v-btn color="primary">Sign Up</v-btn>
       </v-container>
     </v-app-bar>
     <v-main>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,23 @@
 <template>
   <v-container style="max-width: 1080px;">
-    <p class="mb-0">
-      This is a work in progress.
-    </p>
+    <div class="d-flex flex-column align-center my-8 my-sm-16">
+      <h1 class="text-h3 text-sm-h2 font-weight-bold text-center">
+        Gotta do 'em all <br />
+        with TooDoo
+      </h1>
+      <v-btn class="mt-6" color="primary" large>Get Started</v-btn>
+    </div>
+    <div class="d-flex justify-center py-8 my-8 my-sm-16">
+      <v-img :src="require('@/assets/svgs/undraw_to_do_list_a49b.svg')" :aspect-ratio="1" :max-height="480" contain />
+    </div>
+    <div class="d-flex flex-column align-center my-8 my-sm-16">
+      <h1 class="text-h3 text-sm-h2 font-weight-bold text-center">
+        Life shouldn't be chaos.
+      </h1>
+      <p class="body-1 my-8">
+        From business ventures to grocery lists, divide and conquer your daily tasks in shared projects.
+      </p>
+    </div>
   </v-container>
 </template>
 
